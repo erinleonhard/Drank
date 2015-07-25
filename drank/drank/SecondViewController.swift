3777c8792deb54607c9c7f84b1e4b0ca04e1be4a
eeleonha <eeleonha@indiana.edu>
2015-07-25 15:26:54 -0400

    created project

--- drank/drank/SecondViewController.swift
+++ drank/drank/SecondViewController.swift
@@ -2,8 +2,8 @@
 //  SecondViewController.swift
 //  drank
 //
-//  Created by Luke Street on 7/25/15.
-//  Copyright © 2015 xtern. All rights reserved.
+//  Created by Erin Leonhard on 7/25/15.
+//  Copyright (c) 2015 Xtern. All rights reserved.
 //
 
 import UIKit
