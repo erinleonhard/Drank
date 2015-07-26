27940021b86519c86198d438e8c5ead528929763
ldstreet <ldstreet@indiana.edu>
2015-07-26 04:46:38 -0400

    new stuff!

---
+++ drank/HistoryViewController.swift
@@ -0,0 +1,61 @@
+//
+//  HistoryViewController.swift
+//  drank
+//
+//  Created by Luke Street on 7/26/15.
+//  Copyright © 2015 Xtern. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView: UITableView!
+    var drinks: [String] = ["We", "Heart", "Swift"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(drinks.count)
+        return drinks.count;
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = (tableView.dequeueReusableCellWithIdentifier("DrinkLogTableViewCell") as! DrinkLogTableViewCell)
+        print ("Hi: " + self.drinks[indexPath.row])
+        //cell.setDrinkName(self.drinks[indexPath.row])
+        print(cell.drinkNameLabel.text)
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell #\(indexPath.row)!")
+        //performSegueWithIdentifier("toDetails", sender: self)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+
+
+}
