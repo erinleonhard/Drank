28416f9e4a44860d9b941221c7b64bbd32212f9b
eeleonha <eeleonha@indiana.edu>
2015-07-25 20:56:02 -0400

    added sign up and log in views

--- drank/drank/SecondViewController.swift
+++ drank/drank/SecondViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import Parse
 
 class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func signUp() {
+        var user = PFUser()
+        user.username = "myUsername"
+        user.password = "myPassword"
+        user.email = "email@example.com"
+        // other fields can be set just like with PFObject
+        user["phone"] = "415-392-0202"
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error {
+                let errorString = error.userInfo?["error"] as? NSString
+                // Show the errorString somewhere and let the user try again.
+            } else {
+                // Hooray! Let them use the app now.
+            }
+        }
+    }
 
 }
 
